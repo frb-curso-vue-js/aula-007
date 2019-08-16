d2d1d0cdb3ab48c91e5270b46fc8a662680b3bb9
FRamos <framos.df@gmail.com>
2019-08-16 17:20:18 -0300

    Criando repositório. FRB

---
+++ componente-desafio/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+	<v-app>
+		<Menu />
+		<Titulo />
+		<Carousel />
+		<Footer />
+	</v-app>
+</template>
+
+<script>
+import Menu from '@/components/template/App-menu.vue'
+import Titulo from '@/components/template/App-titulo.vue'
+import Carousel from '@/components/widgets/App-carousel.vue'
+import Footer from '@/components/template/App-footer.vue'
+
+export default {
+	components: {
+		Menu,
+		Titulo,
+		Carousel,
+		Footer
+	}
+	
+}
+</script>
---
+++ src/Contadores.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="contadores">
+        <h1>Contadores</h1>
+        <app-contador v-for="c in 20" :key="c" />
+    </div>
+</template>
+
+<script>
+import Contador from './Contador.vue'
+
+export default {
+    components: {
+        'app-contador': Contador
+    }
+}
+</script>
+
+<style scoped>
+
+    div {
+        border: 1px solid blueviolet;
+    }
+
+</style>
